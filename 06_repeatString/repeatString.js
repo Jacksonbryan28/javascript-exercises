f802673db0fe18b4be705e7748bc377ee3c26182
Jackson <jacksonbryan280@gmail.com>
2025-05-07 18:39:11 -0400

    Completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (inputString, repeatNum) {
+  let outputString = "";
+  for (let i = 0; i < repeatNum; i++) {
+    outputString = outputString + inputString;
+  }
+  return outputString;
 };
 
 // Do not edit below this line
