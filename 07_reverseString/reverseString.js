2e58500bb9ba77a6c8b8bf073abbe398ccebc906
Jackson <jacksonbryan280@gmail.com>
2025-05-07 19:35:33 -0400

    Completed exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (getString) {
+  let outputString = "";
+  let getStringLength = getString.length;
+  for (i = getStringLength; i >= 0; i--) {
+    outputString = outputString + getString.charAt(i);
+  }
+  return outputString;
 };
 
 // Do not edit below this line
