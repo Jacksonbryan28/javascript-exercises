6af49d8a743741de7cc601d8e8238df3029c2462
Jackson <jacksonbryan280@gmail.com>
2025-05-08 19:54:25 -0400

    Completed exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -8,11 +8,12 @@ const removeFromArray = function () {
   //runs a loop for however many inputs you get on top of the original array
   for (i = 0; i <= args.length; i++) {
     //checks if at each location it matches the current argument we are checking
-    for (x = 0; x <= nestedArray.length; x++) {
+    for (x = 0; x < nestedArray.length; x++) {
       //if they match, we remove it from that slice
       if (nestedArray[x] === args[i]) {
         nestedArray.splice(x, 1);
-        x = 0;
+        x = --x;
+        console.log(x);
       }
     }
   }
