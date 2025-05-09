ce27cf058523ec84de36c1b116128655656f1c1a
Jackson <jacksonbryan280@gmail.com>
2025-05-08 22:02:27 -0400

    4 tests working for exerices 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  //Defines it as a number
+  let sumOutput = 0;
+  let numOneTest = numOne;
+  let numTwoTest = numTwo;
 
+//   if(numOne !== 'number' || numTwo !== 'number'){
+//     return "ERROR";
+//   }
+//   else
+   if (
+    typeof numOneTest === "number" &&
+    typeof numTwoTest === "number" &&
+    numOne > 0 &&
+    numTwo > 0
+  ) {
+    if (numOne < numTwo) {
+      for (i = numOne; i <= numTwo; i++) {
+        sumOutput += i;
+
+        //Logs to test
+        //   console.log(sumOutput);
+        //   console.log("numOne Value: " + numOne + " numTwo Value: " + numTwo);
+        //   console.log("i Value" + i);
+      }
+    } else {
+      for (i = numTwo; i <= numOne; i++) {
+        sumOutput += i;
+      }
+    }
+    return sumOutput;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
