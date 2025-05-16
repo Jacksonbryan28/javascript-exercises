3dfc6c62b342637a5c8b31899ce9c030d4871aba
Jackson <jacksonbryan280@gmail.com>
2025-05-16 16:26:16 -0400

    Completed exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,42 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (startDeg) {
+  let answer = 0;
+  let unroundedAnswer;
+  //converts the input to celcius
+  unroundedAnswer = ((startDeg - 32) * 5) / 9;
+  //if answer has a decimal place round it to one
+  if (unroundedAnswer % 1 !== 0) {
+    //makes it go to one decimal place
+    answer = Number.parseFloat(unroundedAnswer).toFixed(1);
+    answer = Number(answer);
+  } else {
+    Number(unroundedAnswer);
+    answer = unroundedAnswer;
+  }
+
+  return answer;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (startDeg) {
+  let answer = 0;
+  let unroundedAnswer;
+
+  unroundedAnswer = (startDeg * 9) / 5 + 32;
+
+  //if answer has a decimal place round it to one
+  if (unroundedAnswer % 1 !== 0) {
+    //makes it go to one decimal place
+    answer = Number.parseFloat(unroundedAnswer).toFixed(1);
+    answer = Number(answer);
+  } else {
+    Number(unroundedAnswer);
+    answer = unroundedAnswer;
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
