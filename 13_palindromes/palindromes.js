f708de8582fecb7708f268530dbae6914c61a0a3
Jackson <jacksonbryan280@gmail.com>
2025-07-14 12:19:28 -0400

    completed palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  //remove non-alphabetical characters
+  let cleanString = string.replace(/[^a-zA-Z0-9]/g, "");
+  //set string to lowercase
+  let lowerCase = cleanString.toLowerCase();
+  //reverse string
+  let reverseString = "";
+  for (i = lowerCase.length; i >= 0; i--) {
+    reverseString += lowerCase.charAt(i);
+  }
+  console.log(reverseString);
+  //check if reverse == original string - if true return true, if false, return false
+  if (lowerCase == reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
