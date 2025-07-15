ee9ae16106b1d145130e2c1288b56eb7468cec35
Jackson <jacksonbryan280@gmail.com>
2025-07-15 10:09:36 -0400

    completed fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  if (n < 0) {
+    return "OOPS";
+  }
 
+  let a = 0,
+    b = 1;
+
+  for (i = 2; i <= n; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+  return fib;
 };
 
 // Do not edit below this line
